7a4fcb61ac7ac2dd9a2a8795565b31696fc345b1
Tyrese804 <whiteheadtj@vcu.edu>
2025-04-28 23:20:18 -0400

    updated info

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,9 +2,9 @@ import { RevealOnScroll } from "./RevealOnScroll";
 
 export const About = () => {
 
-    const frontendSkills = ["React", "Vue", "TailwindCSS"]
+    const frontendSkills = ["HTML", "CSS", "JavaScript", "React", "Vue"]
 
-    const backendSkills = ["Python", "Java", "MongoDB"]
+    const backendSkills = ["Python", "Java", "C", "C++", "NodeJS", "Flask", "MongoDB"]
 
 
     return (
@@ -16,9 +16,10 @@ export const About = () => {
                 </h2>
 
                 <div className="glass rounded-xlp-8 border-white/10 border hover:-translate-y-1 transition-all">
-                    <p className="text-gray-300 mb-6">
-                        Passionate developer with expertise in building scalable web applications and creating
-                        innovative solutions.
+                    <p className="text-gray-300 mb-4 pt-4 px-4">
+                    Passionate developer with expertise in building scalable web applications and creating innovative solutions. 
+                    I also have experience working across the stack, including backend development, game development, and system 
+                    design — bringing a versatile and creative approach to every project.
                     </p>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -26,7 +27,7 @@ export const About = () => {
                             <h3 className="text-xl font-bold mb-4">
                                 Frontend
                             </h3>
-                            <div className="flex flex-wrap gap-2">
+                            <div className="flex flex-wrap gap-2 justify-center">
                                 {frontendSkills.map((tech) => (
                                     <span className="bg-red-500/10 text-red-500 py-1 px-3 rounded-full text-sm hover:bg-red-500/20
                                                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transtion">
@@ -40,7 +41,7 @@ export const About = () => {
                             <h3 className="text-xl font-bold mb-4">
                                 Backend
                             </h3>
-                            <div className="flex flex-wrap gap-2">
+                            <div className="flex flex-wrap gap-2 justify-center">
                                 {backendSkills.map((tech) => (
                                     <span className="bg-red-500/10 text-red-500 py-1 px-3 rounded-full text-sm hover:bg-red-500/20
                                                      hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transtion">
@@ -60,7 +61,7 @@ export const About = () => {
                                 <strong>B.S. in Compiuter Science </strong> - Virginia Commonwealth University (2021-2024)
                             </li>
                             <li>
-                                Relevant Coursework: Data Structures, Web Development, Computational Theory, ...
+                                Relevant Coursework: Intro to Programming, Data Structures and Algorithms, Computer Systems, Intro to Discrete Structures, Computer Organization, Intro to Theory of Computation, Fundamentals of Software Engineering, Web Development
                             </li>
                         </ul>
 
@@ -73,20 +74,21 @@ export const About = () => {
 
                         <div className="space-y-4 text-gray-300">
                             <div>
-                                <h4 className="font-semibold">
-                                    Technology Analyst at Wells Fargo (2025 - present)
+                                <h4 className="font-semibold underline">
+                                    Technology Analyst at Wells Fargo (2024 - present)
                                 </h4>
                                 <p>
-                                    blah blah blah
+                                Contributed to modernizing a large legacy application by converting it to a component-based UI, streamlining the process for implementing future updates. 
+                                Also, enhanced application performance through memorization, added key functionalities, and ensured UI alignment with Figma designs.
                                 </p>
                             </div>
 
                             <div>
-                                <h4 className="font-semibold">
+                                <h4 className="font-semibold underline">
                                     Research Assistant at VCU (2023 - 2024)
                                 </h4>
                                 <p>
-                                    blah blah blah
+                                Developed a Python application to perform over 1,000 of calculations based on various parameters, automating a previously manual process of generating Excel files and storing data for input into a machine learning application.
                                 </p>
                             </div>
 
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -11,13 +11,14 @@ export const Projects = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-red-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transtion">
                         <h3 className="text-xl font-bold mb-2">
-                            Project1
+                            AI Chatbot
                         </h3>
                         <p className="text-gray-400 mb-4">
-                            project description...
+                        Created an intelligent AI chatbot using neural networks and 
+                        natural language processing in Python to provide accurate responses to user inquiries about me.
                         </p>
-                        <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Node.js", "Docker"].map((tech, key) => (
+                        <div className="flex flex-wrap gap-2 mb-4 justify-center">
+                            {["Python", "Natural Language Processing", "Json"].map((tech, key) => (
                                 <span className="bg-red-500/10 text-red-500 py-1 px-3 rounded-full text-sm hover:bg-red-500/20
                                 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transtion-all">
                                     {tech}
@@ -25,8 +26,30 @@ export const Projects = () => {
                             ))}
                         </div>
 
-                        <div className="flex justify-between items-center">
-                            <a href="#" className="text-red-400 hover:text-red-300 transition-colors my-4">View Project</a>
+                        <div className="flex justify-between justify-center">
+                            <a href="https://github.com/Tyrese804/AI-Chatbot" target="_blank" className="text-red-400 hover:text-red-300 transition-colors my-4">View Project</a>
+                        </div>
+                    </div>
+
+                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-red-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transtion">
+                        <h3 className="text-xl font-bold mb-2">
+                            Pong
+                        </h3>
+                        <p className="text-gray-400 mb-4">
+                        Developed a multiplayer Pong game using the PyGame library in Python.
+                        Implemented collision handling and utilized concise and scalable code for an enjoyable gaming experience.
+                        </p>
+                        <div className="flex flex-wrap gap-2 mb-4 justify-center">
+                            {["Python", "PyGame"].map((tech, key) => (
+                                <span className="bg-red-500/10 text-red-500 py-1 px-3 rounded-full text-sm hover:bg-red-500/20
+                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transtion-all">
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+
+                        <div className="flex justify-between justify-center">
+                            <a href="https://github.com/Tyrese804/Pong-Game" target="_blank" className="text-red-400 hover:text-red-300 transition-colors my-4">View Project</a>
                         </div>
                     </div>
 
